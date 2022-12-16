e6843977a7ba16f9d66109b6104f80ce81e8bb8d
sutep_jantawee <suthep@vertobase.com>
2022-12-17 04:24:42 +0700

    add file setting

---
+++ lib/setting_view/setting_security.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SettingSecurity extends StatefulWidget {
+  const SettingSecurity({super.key});
+
+  @override
+  State<SettingSecurity> createState() => _SettingSecurityState();
+}
+
+class _SettingSecurityState extends State<SettingSecurity> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ElevatedButton(
+        onPressed: () {},
+        child: null,
+      ),
+    );
+  }
+}
